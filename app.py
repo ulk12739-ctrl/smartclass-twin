74a48e430481b5206886a26783a1ff3dc5214363
ulk12739-ctrl <ulk12739@gmail.com>
2026-06-21 23:27:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with col_sol:
     st.image("kesapfenlogo-removebg-preview.png", width=90)
 
 with col_sag:
-    st.image("meblogo-removebg-preview.png", width=200)
+    st.image("meblogo-removebg-preview.png", width=300)
 
 st.title("🏫 SmartClass Twin - Öğrenci Risk Analiz Paneli")
 st.write("Öğrencinin verilerini girerek yapay zeka ve ağırlıklı risk puanı analizini anında görebilirsiniz.")
